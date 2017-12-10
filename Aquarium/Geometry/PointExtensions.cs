8c240b51660d6a9f3f182cb9969f6dccdbc051be
Sergey Kuzminov <derarvend@gmail.com>
2017-12-11 01:57:52 +0500

    add geometry prototypes

---
+++ Aquarium/Geometry/PointExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+
+namespace Aquarium.Geometry
+{
+	public static class PointExtensions
+	{
+		public static double GetDistanceTo(this Point p1, Point p2)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static (double length, double angle) GetPolarCoordinates(this Point p)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Aquarium/Geometry/Vector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+
+namespace Aquarium.Geometry
+{
+	public class Vector
+	{
+		public readonly double X;
+		public readonly double Y;
+
+		public Vector(Point p)
+		{
+			X = p.X;
+			Y = p.Y;
+		}
+
+		public Vector(double length, double angle)
+		{
+			X = length * Math.Acos(angle);
+			Y = length * Math.Asin(angle);
+		}
+
+		//TODO: Write other methods
+	}
+}
