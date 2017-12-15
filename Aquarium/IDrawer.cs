5b1bce92b1e27386b621b0dfcc2519c5899f8361
Sergey Kuzminov <derarvend@gmail.com>
2017-12-15 15:39:53 +0500

    add VisualisationDrawer

---
+++ Aquarium/IDrawer.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+using Aquarium.Fishes;
+
+namespace Aquarium
+{
+	public interface IDrawer
+	{
+		 Bitmap GetVisualisation(IGame game);
+	}
+}
---
+++ Aquarium/VisualisatonDrawer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+using Aquarium.Fishes;
+
+namespace Aquarium
+{
+	public class VisualisatonDrawer : IDrawer
+	{
+		public Bitmap GetVisualisation(IGame game)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
